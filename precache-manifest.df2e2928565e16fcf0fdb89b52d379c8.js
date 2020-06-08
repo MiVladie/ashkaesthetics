c8c0d9963a07661d76ed0fb5b06a080d23209ebf
MiVladie <justvladon@outlook.com>
2020-06-08 14:12:58 +0100

    Updates

---
+++ precache-manifest.df2e2928565e16fcf0fdb89b52d379c8.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c568ae1d24076f58df810fdca67070e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dafaebd9e18396bc6802",
+    "url": "/static/css/main.1108e9fd.chunk.css"
+  },
+  {
+    "revision": "28143ca88b6903f74ae0",
+    "url": "/static/js/2.30f2d1b7.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/static/js/2.30f2d1b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dafaebd9e18396bc6802",
+    "url": "/static/js/main.bed8cc85.chunk.js"
+  },
+  {
+    "revision": "c775272e315b3764768c",
+    "url": "/static/js/runtime-main.b7449702.js"
+  },
+  {
+    "revision": "224c62d5ed06e4c6872b57bc54c6cc10",
+    "url": "/static/media/1.224c62d5.jpg"
+  },
+  {
+    "revision": "d0722437b4215e397df33c45e368bb7f",
+    "url": "/static/media/2.d0722437.jpg"
+  },
+  {
+    "revision": "819fdc96b5a50622bc93a91a4af84eab",
+    "url": "/static/media/3.819fdc96.jpg"
+  },
+  {
+    "revision": "8efcd1382c918bbf78c776ecee0088f9",
+    "url": "/static/media/4.8efcd138.jpg"
+  },
+  {
+    "revision": "8ca8f6d505d5a31b646bffa8b169e212",
+    "url": "/static/media/5.8ca8f6d5.jpg"
+  },
+  {
+    "revision": "274415579846fc81d35be8c453d01d09",
+    "url": "/static/media/6.27441557.jpg"
+  },
+  {
+    "revision": "c390105fb37f73cbe0bd61c99f16fef0",
+    "url": "/static/media/7.c390105f.jpg"
+  },
+  {
+    "revision": "1df87984addadcf9b7af6cd41419eec9",
+    "url": "/static/media/8.1df87984.jpg"
+  },
+  {
+    "revision": "482423a212b133ba6a72a2eda95436b4",
+    "url": "/static/media/9.482423a2.jpg"
+  },
+  {
+    "revision": "56ac8eb9e0b51324fd47896691675965",
+    "url": "/static/media/brows and lash.56ac8eb9.png"
+  },
+  {
+    "revision": "f32c624b284022e622c0eee79e7991af",
+    "url": "/static/media/eyes & brows 2.f32c624b.jpg"
+  },
+  {
+    "revision": "0e9d414e685342788be2ddeef1d282d4",
+    "url": "/static/media/eyes & brows.0e9d414e.jpg"
+  },
+  {
+    "revision": "3f0b4435c1d6717f4490f32183e2c0ba",
+    "url": "/static/media/logo.3f0b4435.png"
+  },
+  {
+    "revision": "922d4acb154333a16bca1bc414e813e8",
+    "url": "/static/media/mens treatments.922d4acb.png"
+  },
+  {
+    "revision": "955d329bea1db5742cb99baca2e157fb",
+    "url": "/static/media/mens treatments.955d329b.jpg"
+  },
+  {
+    "revision": "138ad3e5feb842f2c2d05d4dcfd57f31",
+    "url": "/static/media/microblading.138ad3e5.jpg"
+  },
+  {
+    "revision": "d764724ece33d13a820563e20b9e9e9e",
+    "url": "/static/media/microblading.d764724e.png"
+  },
+  {
+    "revision": "a1db064d0ffbf53dc9c525b8122bc6cb",
+    "url": "/static/media/microblading02.a1db064d.jpg"
+  },
+  {
+    "revision": "3e0decdca8277250d60ba3bc2bfe6b47",
+    "url": "/static/media/packages.3e0decdc.jpg"
+  },
+  {
+    "revision": "a3314eddcfd58af56b4900baa68290a4",
+    "url": "/static/media/page_404.a3314edd.jpg"
+  },
+  {
+    "revision": "0d8c42aee7fe51de51b56d86390e3e32",
+    "url": "/static/media/page_contact.0d8c42ae.jpg"
+  },
+  {
+    "revision": "1ac79e252d6f1e63d7fff57350661886",
+    "url": "/static/media/page_gallery.1ac79e25.jpg"
+  },
+  {
+    "revision": "db98a300667c28b78c3fb4777d687956",
+    "url": "/static/media/page_home.db98a300.jpg"
+  },
+  {
+    "revision": "57d1ab52ca2fba54b5e1ad3e56ffa2b7",
+    "url": "/static/media/page_services.57d1ab52.jpg"
+  },
+  {
+    "revision": "5a15ed9fbc262855ff9edf9e33d6ab63",
+    "url": "/static/media/waxing.5a15ed9f.png"
+  },
+  {
+    "revision": "c0eed510ede1238af9aa445c041a73af",
+    "url": "/static/media/waxing.c0eed510.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ashkaesthetics/precache-manifest.7fcb43ab3bfd24418b6ad5f8711aad42.js"
+  "/precache-manifest.df2e2928565e16fcf0fdb89b52d379c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ashkaesthetics/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
